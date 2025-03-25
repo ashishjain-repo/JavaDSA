20e05ee9924748cfc68e9c5505e8665d1cd86ac3
Ashish Jain <ashishjain99@yahoo.com>
2025-03-25 12:28:18 -0600

    Added BigO(n)

---
+++ misc/BigO[n]/Main.java
@@ -0,0 +1,12 @@
+// Big O with time complexisty of O(n)
+class Main{
+    public static void main(String[] args){
+        bigOn(10);
+    }
+    public static void bigOn(int n){
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
