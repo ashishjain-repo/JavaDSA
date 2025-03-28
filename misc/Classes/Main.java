4fc3c2210fd29f0ce0bcf265f1f9b6f7b643b208
Ashish Jain <ashishjain99@yahoo.com>
2025-03-27 20:07:50 -0600

    Added Classes and Pointers

---
+++ misc/Classes/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        Cookie cookieOne = new Cookie("Green");
+        Cookie cookieTwo = new Cookie("Blue");
+
+        cookieOne.setColor("Yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+    }
+}
